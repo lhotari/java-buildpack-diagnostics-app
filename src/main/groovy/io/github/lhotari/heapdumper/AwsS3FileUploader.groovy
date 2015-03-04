f3edf52fdf1f608d5efe36be0605b4946547d5a2
Lari Hotari <lhotari@users.noreply.github.com>
2015-03-04 08:21:34 -0500

    calculate content-length before transfering

--- src/main/groovy/io/github/lhotari/heapdumper/AwsS3FileUploader.groovy
+++ src/main/groovy/io/github/lhotari/heapdumper/AwsS3FileUploader.groovy
@@ -6,6 +6,11 @@ import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest
 import com.amazonaws.services.s3.model.ObjectMetadata
 import com.amazonaws.services.s3.model.PutObjectRequest
+import groovy.transform.CompileStatic
+import org.apache.commons.io.IOUtils
+import org.apache.commons.io.input.CountingInputStream
+import org.apache.commons.io.output.CountingOutputStream
+import org.apache.commons.io.output.NullOutputStream
 import org.joda.time.LocalDateTime
 import org.mule.util.compression.GZIPCompressorInputStream
 
@@ -14,6 +19,7 @@ import org.mule.util.compression.GZIPCompressorInputStream
  *
  * Created by lari on 03/03/15.
  */
+@CompileStatic
 class AwsS3FileUploader {
     AmazonS3Client s3Client
     String bucketName
@@ -37,10 +43,36 @@ class AwsS3FileUploader {
         return file.name
     }
 
-    String gzipAndUploadFile(File file) {
+    String gzipAndUploadFile(File file, boolean noTempFile = true) {
         String name =  "${file.name}.gz"
-        s3Client.putObject(new PutObjectRequest(bucketName, name, new GZIPCompressorInputStream(file.newInputStream()), new ObjectMetadata()))
+        if(noTempFile) {
+            long contentLength = calculateGzippedLength(file)
+            def metadata = new ObjectMetadata()
+            metadata.setContentLength(contentLength)
+            s3Client.putObject(new PutObjectRequest(bucketName, name, createGzipCompressingInputStream(file), metadata))
+        } else {
+            File tempFile = new File(file.getParentFile(), name)
+            try {
+                tempFile.withOutputStream {
+                    OutputStream output
+                    IOUtils.copy(createGzipCompressingInputStream(file), output)
+                }
+                s3Client.putObject(new PutObjectRequest(bucketName, name, tempFile))
+            } finally {
+                tempFile.delete()
+            }
+        }
         return name
+    }
+
+    long calculateGzippedLength(File file) {
+        CountingOutputStream countingOutput = new CountingOutputStream(new NullOutputStream())
+        IOUtils.copy(createGzipCompressingInputStream(file), countingOutput)
+        countingOutput.getByteCount()
+    }
+
+    protected GZIPCompressorInputStream createGzipCompressingInputStream(File file) {
+        new GZIPCompressorInputStream(file.newInputStream())
     }
 
     String generatePresignedUrl(String objectKey) {
