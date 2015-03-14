59bd1a9ba816e408d03016dbe3f13dc98c7defb1
Lari Hotari <lhotari@users.noreply.github.com>
2015-03-13 20:25:57 -0400

    add servlet for providing lsof information

---
+++ src/main/groovy/io/github/lhotari/jbpdiagnostics/ListOfOpenFilesServlet.groovy
@@ -0,0 +1,33 @@
+package io.github.lhotari.jbpdiagnostics
+
+import javax.servlet.GenericServlet
+import javax.servlet.ServletException
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServletResponse
+
+/**
+ * Servlet for accessing lsof
+ *
+ * Created by lari on 03/03/15.
+ */
+@WebServlet("/lsof")
+class ListOfOpenFilesServlet extends GenericServlet {
+    @Override
+    void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        def out=((HttpServletResponse)res).getWriter()
+        if(AccessControlService.instance.isOperationAllowed(req, "lsof")) {
+            doLsof(out)
+        } else {
+            out << "NOT OK"
+        }
+        out.close()
+    }
+
+    protected synchronized void doLsof(PrintWriter out) {
+        def lsofOutput = "lsof -p ${DiagUtils.currentProcessId}".execute().text
+        out << lsofOutput
+    }
+}
+
