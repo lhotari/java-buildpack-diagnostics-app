d790cc9ba1303560a8124241d1c15187c3ac4b41
Lari Hotari <lhotari@users.noreply.github.com>
2015-03-13 20:26:21 -0400

    add servlet that provides netstat information

---
+++ src/main/groovy/io/github/lhotari/jbpdiagnostics/NetstatServlet.groovy
@@ -0,0 +1,36 @@
+package io.github.lhotari.jbpdiagnostics
+
+import javax.servlet.GenericServlet
+import javax.servlet.ServletException
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServletResponse
+
+/**
+ * Servlet for accessing netstat
+ *
+ * Created by lari on 03/03/15.
+ */
+@WebServlet("/netstat")
+class NetstatServlet extends GenericServlet {
+    @Override
+    void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        def out=((HttpServletResponse)res).getWriter()
+        if(AccessControlService.instance.isOperationAllowed(req, "netstat")) {
+            doNetstat(req, out)
+        } else {
+            out << "NOT OK"
+        }
+        out.close()
+    }
+
+    protected synchronized void doNetstat(ServletRequest req, PrintWriter out) {
+        def params = "-anp"
+        if(req.getParameter("stat")) {
+            params = "-s"
+        }
+        out << "netstat ${params}".execute().text
+    }
+}
+
